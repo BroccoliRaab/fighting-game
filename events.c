0bf0c0742aa7fe2fdc31cbd56cb6c501f1367a07
Broccoliraab <broccoliraab@protonmail.com>
2020-10-06 01:40:01 +0000

    Added default case for event handling

--- events.c
+++ events.c
@@ -9,8 +9,10 @@ int handle_events(){
     while (SDL_PollEvent(&event_handler) != 0){
         switch(event_handler.type){
             case SDL_QUIT:
-                 return 1;
+                return 1;
                 break;
+            default:
+                SDL_LogWarn(SDL_LOG_CATEGORY_APPLICATION, "UNHANDLED EVENT: %s\n", event_handler.type);
         }
     }
     return 0;
